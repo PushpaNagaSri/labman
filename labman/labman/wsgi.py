a77a4b9b1efa5b1a4dfc10504d26afb235289e6f
PushpaNagaSri <pushpanagasrivangara.gmail.com>
2024-11-09 20:55:12 +0530

    com7

--- labman/labman/wsgi.py
+++ labman/labman/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+import sys
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'labman'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labman.settings')
 
